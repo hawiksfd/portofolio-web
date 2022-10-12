00158a1eb139e938f3cb0ee20dc94e275a27bc2c
hawik <hawiksyaef@gmail.com>
2022-10-12 15:08:21 +0700

    styling navbar logo

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,27 @@
 import './navbar.scss'
+import ContactMailIcon from '@mui/icons-material/ContactMail';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      navbar
+      <div className="wrapper">
+        <div className="left">
+          <a href='#header' className='logo'>.HAWIKSFD</a>
+          <div className="itemContainer">
+            <ContactMailIcon className='icon'/>
+            <a href='#contact' className='title'>mohhawik17@gmail.com</a>
+          </div>
+          <div className="itemContainer">
+            <GitHubIcon className='icon'/>
+            <a href='https://github.com/hawiksfd' target="_blank" className='title' rel="noreferrer">hawiksfd</a>
+          </div>
+        </div>
+        <div className="right">
+          kanan
+        </div>
+      </div>
+      
     </div>
   )
 }
