1b407b4a12f38cad12d74d86587ffc7b4f25e967
hawik <hawiksyaef@gmail.com>
2022-10-19 10:42:48 +0700

    custom portofolio page

--- src/components/portofolio/Portofolio.jsx
+++ src/components/portofolio/Portofolio.jsx
@@ -1,9 +1,48 @@
 import './portofolio.scss'
+import App from './../../App';
 
 const Portofolio = () => {
   return (
     <div className='portofolio' id='portofolio'>
-      
+      <h1>Portofolio</h1>
+      <ul>
+        <li className='active'>Web App</li>
+        <li>Mobile App</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="https://www.softwareistic.com/assets/images/banking.png" alt="" />
+          <h3>Banking Apps</h3>
+        </div>
+        <div className="item">
+          <img src="https://www.softwareistic.com/assets/images/banking.png" alt="" />
+          <h3>Banking Apps</h3>
+        </div>
+        <div className="item">
+          <img src="https://www.softwareistic.com/assets/images/banking.png" alt="" />
+          <h3>Banking Apps</h3>
+        </div>
+        <div className="item">
+          <img src="https://www.softwareistic.com/assets/images/banking.png" alt="" />
+          <h3>Banking Apps</h3>
+        </div>
+        <div className="item">
+          <img src="https://www.softwareistic.com/assets/images/banking.png" alt="" />
+          <h3>Banking Apps</h3>
+        </div>
+        <div className="item">
+          <img src="https://www.softwareistic.com/assets/images/banking.png" alt="" />
+          <h3>Banking Apps</h3>
+        </div>
+        <div className="item">
+          <img src="https://www.softwareistic.com/assets/images/banking.png" alt="" />
+          <h3>Banking Apps</h3>
+        </div>
+        <div className="item">
+          <img src="https://www.softwareistic.com/assets/images/banking.png" alt="" />
+          <h3>Banking Apps</h3>
+        </div>
+      </div>
     </div>
   )
 }
