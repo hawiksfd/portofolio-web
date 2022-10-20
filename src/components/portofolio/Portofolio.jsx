f134394a392847982ca004c2ca6f909aef1efbaa
hawik <hawiksyaef@gmail.com>
2022-10-20 10:30:51 +0700

    dynamic data porto with useEffect hook

--- src/components/portofolio/Portofolio.jsx
+++ src/components/portofolio/Portofolio.jsx
@@ -1,10 +1,12 @@
 import './portofolio.scss'
 import PortoList from './PortoList';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { featuredPorto, webPorto, mobilePorto } from '../../data.js'
 
 const Portofolio = () => {
 
-  const [selected, setSelected] = useState('featured')
+  const [selected, setSelected] = useState('featured');
+  const [data, setData] = useState([]);
   const list = [
     {
       id: 'featured',
@@ -18,53 +20,45 @@ const Portofolio = () => {
       id: 'mobile',
       title: 'Mobile Apps'
     }
-  ]
+  ];
+
+  useEffect(()=>{
+    switch(selected){
+      case 'featured' :
+        setData(featuredPorto);
+        break;
+      case 'web' :
+        setData(webPorto);
+        break;
+      case 'mobile' :
+        setData(mobilePorto);
+        break;
+      default :
+        setData(featuredPorto);
+    }
+  },[selected])
+
   return (
     <div className='portofolio' id='portofolio'>
       <h1>Portofolio</h1>
       <ul>
-        {list.map(item => (
+        {list.map((item) => (
           <PortoList 
             title={item.title}
             active={selected === item.id}
             setSelected={setSelected}
             id={item.id}
+            key={item.id}
           />
         ))}
       </ul>
       <div className="container">
-        <div className="item">
-          <img src="https://www.softwareistic.com/assets/images/banking.png" alt="" />
-          <h3>Banking Apps</h3>
-        </div>
-        <div className="item">
-          <img src="https://www.softwareistic.com/assets/images/banking.png" alt="" />
-          <h3>Banking Apps</h3>
-        </div>
-        <div className="item">
-          <img src="https://www.softwareistic.com/assets/images/banking.png" alt="" />
-          <h3>Banking Apps</h3>
-        </div>
-        <div className="item">
-          <img src="https://www.softwareistic.com/assets/images/banking.png" alt="" />
-          <h3>Banking Apps</h3>
-        </div>
-        <div className="item">
-          <img src="https://www.softwareistic.com/assets/images/banking.png" alt="" />
-          <h3>Banking Apps</h3>
-        </div>
-        <div className="item">
-          <img src="https://www.softwareistic.com/assets/images/banking.png" alt="" />
-          <h3>Banking Apps</h3>
-        </div>
-        <div className="item">
-          <img src="https://www.softwareistic.com/assets/images/banking.png" alt="" />
-          <h3>Banking Apps</h3>
-        </div>
-        <div className="item">
-          <img src="https://www.softwareistic.com/assets/images/banking.png" alt="" />
-          <h3>Banking Apps</h3>
-        </div>
+        {data.map((datas) => (
+          <div className="item" key={datas.id} >
+            <img src={datas.img} alt="" />
+            <h3>{datas.title}</h3>
+          </div>
+        ))}
       </div>
     </div>
   )
